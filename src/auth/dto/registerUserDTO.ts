ee771f9f88b91c457c16e258224d7348ddaf1c41
Manish <manish@Preethis-MacBook-Pro.local>
2026-02-15 08:17:52 +0000

    refactor registerUserDTO to use validaotor instead of zod

--- src/auth/dto/registerUserDTO.ts
+++ src/auth/dto/registerUserDTO.ts
@@ -1,6 +1,16 @@
+import { IsString } from "class-validator";
+
 export class RegisterUserDTO {
+    @IsString()
     firstName?: string;
+
+    @IsString()
     lastName?: string;
+
+    @IsString()
     passwordHashed?: string;
+
+    @IsString()
     email?: string;
 }
+
